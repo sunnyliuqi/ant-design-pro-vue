7e44e5ee843ec6e885db8b3c8839ad5c9f3ff0ad
liuqi <710518803@qq.com>
2020-12-18 10:18:16 +0800

    富文本支持

--- public/UEditor/ueditor.config.js
+++ public/UEditor/ueditor.config.js
@@ -43,7 +43,7 @@
             'simpleupload', 'emotion', 'insertframe', 'insertcode', '|',
             'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
             'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', '|',
-            'preview', 'searchreplace'
+            'searchreplace'
         ]],
         // 当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //, labelMap:{

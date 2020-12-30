f057080a1e1e933af564d6b9e93176e51b172a6d
liuqi <710518803@qq.com>
2020-12-30 09:22:51 +0800

    代码优化

--- src/components/Activiti/Bpmn/BpmnDesign.vue
+++ src/components/Activiti/Bpmn/BpmnDesign.vue
@@ -20,7 +20,7 @@
   import propertiesPanelModule from 'bpmn-js-properties-panel-activiti'
   import propertiesProviderModule from 'bpmn-js-properties-panel-activiti/lib/provider/activiti'
   import camundaModdleDescriptor from 'activiti-bpmn-moddle/resources/activiti'
-
+  import { emptyBpmn } from './store/defaultBpmn'
   export default {
     name: 'BpmnDesign',
     props: {
@@ -38,16 +38,7 @@
     data () {
       return {
         bpmnModeler: null,
-        defaultXml: `
-    <?xml version="1.0" encoding="UTF-8"?>
-      <bpmn2:definitions xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" id="sample-diagram" targetNamespace="http://bpmn.io/schema/bpmn" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd">
-        <bpmn2:process id="process1567044459787" name="process1567044459787">
-        </bpmn2:process>
-        <bpmndi:BPMNDiagram id="BPMNDiagram_1">
-          <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="process1567044459787" />
-        </bpmndi:BPMNDiagram>
-      </bpmn2:definitions>
-    `
+        defaultXml: emptyBpmn
       }
     },
     mounted () {
---
+++ src/components/Activiti/Bpmn/store/defaultBpmn.js
@@ -0,0 +1,10 @@
+export const emptyBpmn = `
+    <?xml version="1.0" encoding="UTF-8"?>
+      <bpmn2:definitions xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" id="sample-diagram" targetNamespace="http://bpmn.io/schema/bpmn" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd">
+        <bpmn2:process id="process1567044459787" name="process1567044459787">
+        </bpmn2:process>
+        <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+          <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="process1567044459787" />
+        </bpmndi:BPMNDiagram>
+      </bpmn2:definitions>
+    `

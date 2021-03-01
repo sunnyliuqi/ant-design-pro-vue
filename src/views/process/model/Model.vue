1719bcd6f041e4d1dc115cc4cfc21d4a2301b025
liuqi <710518803@qq.com>
2021-03-01 09:32:20 +0800

    no commit

--- src/views/process/model/Model.vue
+++ src/views/process/model/Model.vue
@@ -23,39 +23,9 @@ export default {
       formLists: [],
       userLists: [],
       groupLists: [],
-      xml: `<?xml version="1.0" encoding="UTF-8"?>
-<bpmn2:definitions xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:activiti="http://activiti.org/bpmn" id="sample-diagram" targetNamespace="http://bpmn.io/schema/bpmn" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd">
-  <bpmn2:process id="process1567044459787" name="process15670444597879" author="111" version="1111">
-    <bpmn2:documentation>663</bpmn2:documentation>
-    <bpmn2:startEvent id="Event_12q4srm" name="开始">
-      <bpmn2:documentation>77</bpmn2:documentation>
-      <bpmn2:extensionElements>
-      <activiti:executionListener expression="s2" class="s2" delegateExpression="s2" event="start">
-        <activiti:field name="s1" stringValue="s2">
-          <activiti:expression>sd</activiti:expression>
-          <activiti:string>sc</activiti:string>
-        </activiti:field>
-      </activiti:executionListener>
-      <activiti:formProperty id="fid" name="2" type="long" required="true" readable="true" writable="true" variable="fvariable" expression="fexpression" datePattern="yyyy-HH-mm" default="2021-01-12">
-          <activiti:value id="v1" name="v11">v111</activiti:value>
-          <activiti:value id="v2" name="v22">v222</activiti:value>
-        </activiti:formProperty>
-    </bpmn2:extensionElements>
-    <bpmn2:messageEventDefinition id="MessageEventDefinition_1qce8us" activiti:messageRef="myMessage" />
-    </bpmn2:startEvent>
-    <activiti:signals id="theSignal" name="The Signal" scop="processInstance" />
-    <activiti:signals id="theSignal2" name="The Signal2" scop="processInstance2" />
-    <activiti:messages id="theMessage" name="The Message" />
-    <activiti:messages id="theMessage2" name="The Message2" />
-  </bpmn2:process>
-  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
-    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="process1567044459787">
-      <bpmndi:BPMNShape id="Event_12q4srm_di" bpmnElement="Event_12q4srm">
-        <dc:Bounds x="522" y="332" width="36" height="36" />
-      </bpmndi:BPMNShape>
-    </bpmndi:BPMNPlane>
-  </bpmndi:BPMNDiagram>
-</bpmn2:definitions>`
+      xml: undefined,
+      modelKey: undefined,
+      description: undefined
     }
   },
   created () {

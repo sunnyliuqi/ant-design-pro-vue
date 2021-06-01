d91e7ad684751372fc4e6e5d3c081bd79560599a
liuqi <710518803@qq.com>
2021-06-01 18:00:51 +0800

    权限增强

--- src/api/report/load.js
+++ src/api/report/load.js
@@ -21,12 +21,15 @@ export function findProxyUrl (data) {
 export function loadProxyUrl (proxyUrl) {
   const pattern = /(.*)?\?(.*)/
   let _proxyUrl
+  let contextPath = proxyUrl
   if (pattern.test(proxyUrl)) {
+    contextPath = proxyUrl.replace(pattern, '$1')
     const _params = proxyUrl.replace(pattern, '$2')
     _proxyUrl = queryString.parse(_params)
   } else {
     _proxyUrl = {}
   }
   _proxyUrl.Authorization = storage.get(ACCESS_TOKEN)
-  return process.env.VUE_APP_API_BASE_URL + path.report + '?' + queryString.stringify(_proxyUrl)
+  const fullPath = process.env.VUE_APP_API_BASE_URL + path.report + contextPath + '?' + queryString.stringify(_proxyUrl)
+  return fullPath
 }
